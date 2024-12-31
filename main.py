7246ad04c5435f15bf5f06c27c0ce5c0bcc57bf8
Sammy <bavesh2006kumar@gmail.com>
2024-12-31 19:42:51 +0000

    all lib for the project

---
+++ main.py
@@ -0,0 +1,9 @@
+from plyer import notification
+import pyttsx3 
+import speech_recognition as sr
+import pyjokes
+import datetime
+import webbrowser
+import random
+import datetime
+import pyautogui
