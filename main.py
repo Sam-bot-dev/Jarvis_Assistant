e5bfca7a075f9c5945a5da12dacfd5412ab90aa9
Sammy <bavesh2006kumar@gmail.com>
2025-01-03 17:24:41 +0000

    defined speak and greet

--- main.py
+++ main.py
@@ -9,3 +9,45 @@ import datetime
 import pyautogui
 #lib
 engine= pyttsx3.init()# to initiate the engine.
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
+def greet():
+  hour=int(datetime.datetime.now().hour)
+  if hour<12:
+    speak("Good Morning")
+    print("Good Morning")
+  elif hour<18:
+    speak("Good Afternoon")
+    print("Good Afternoon")
+  else:
+    speak("Good Evening")
+    print("Good Evening")
+    speak("I am Jarvis, your personal assistant. How can I help you today?")
+    print("I am Jarvis, your personal assistant. How can I help you today?")
+
+def command():
+  content=" "
+  while content ==" ":
+    r=sr.recognizer()
+    with sr.microphone() as source:
+      r.pause_threshold=1
+      r.energy_threshold=4000
+      r.dynamic_enegy_threshold=True
+      print("say somthing")
+      audio=r.listen(source)
+    try:
+      content=r.recognize_google(audio,language="en-IN")
+      print("you said....  " + content )
+      return content
+    except sr.RequestError as e:
+      print("please try again")
+
+def main_process():
+  greet()
+  url_yt="https://www.youtube.com/"
+  url_g="https://www.google.co.in/"
+  url_github="https://github.com/"
+  while True:
+    pass
+    
